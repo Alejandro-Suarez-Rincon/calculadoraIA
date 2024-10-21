d013a4718dea06176f02bf00f0bc4e5a1a258a5c
Alejandro <alejandrosuarez2032002@gmail.com>
2024-10-20 22:49:18 -0500

    refactor: se refactoriza todo el codigo de Redimencionar

--- Preparacion/Redimencionar.py
+++ Preparacion/Redimencionar.py
@@ -1,25 +1,37 @@
 import os
-
 from PIL import Image
+from pathlib import Path
 
-# Ruta del directorio que contiene las imágenes
-directorio_imagenes = 'C:/Alejandro/Programacion/Python/calculadoraIA/Modelos/DataSetOperaciones/2'
+# Obtener la ruta base del archivo actual
+base_dir = Path(__file__).resolve().parent
+
+# Ruta del directorio base que contiene las subcarpetas con las imágenes
+directorio_base = base_dir / '../Modelos/DataSetOperaciones/'
 
 # Tamaño deseado para las imágenes redimensionadas
 nuevo_ancho = 28
 nuevo_alto = 28
 
-# Recorre todas las imágenes en el directorio
-for nombre_archivo in os.listdir(directorio_imagenes):
-    # Verifica si el archivo es una imagen (extensión común)
-    if nombre_archivo.endswith(".jpg") or nombre_archivo.endswith(".jpeg") or nombre_archivo.endswith(".png"):
-        ruta_imagen = os.path.join(directorio_imagenes, nombre_archivo)
+# Obtener la lista de subcarpetas en el directorio base
+subcarpetas = [f for f in os.listdir(directorio_base) if os.path.isdir(directorio_base / f)]
 
-        # Abre la imagen utilizando Pillow
-        imagen = Image.open(ruta_imagen)
+# Iterar sobre cada subcarpeta
+for subcarpeta in subcarpetas:
+    ruta_subcarpeta = directorio_base / subcarpeta
 
-        # Redimensiona la imagen
-        imagen_redimensionada = imagen.resize((nuevo_ancho, nuevo_alto))
+    # Recorre todas las imágenes en la subcarpeta
+    for nombre_archivo in os.listdir(ruta_subcarpeta):
+        # Verifica si el archivo es una imagen (extensión común)
+        if nombre_archivo.endswith((".jpg", ".jpeg", ".png")):
+            ruta_imagen = ruta_subcarpeta / nombre_archivo
 
-        # Guarda la imagen redimensionada (sobreescribe la imagen original)
-        imagen_redimensionada.save(ruta_imagen)
+            # Abre la imagen utilizando Pillow
+            imagen = Image.open(ruta_imagen)
+
+            # Redimensiona la imagen
+            imagen_redimensionada = imagen.resize((nuevo_ancho, nuevo_alto))
+
+            # Guarda la imagen redimensionada (sobrescribe la imagen original)
+            imagen_redimensionada.save(ruta_imagen)
+
+    print(f"Imágenes redimensionadas en la carpeta: {subcarpeta}")
